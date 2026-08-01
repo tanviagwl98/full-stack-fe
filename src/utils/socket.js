d9e107459b5f40cab93859d74772103838fc75d1
Tanvi Agarwal <vini98agwl@gmail.com>
2026-08-01 13:42:18 +0530

    socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,7 +5,7 @@ export const createSocketConnection = () => {
     if(location.hostname === "localhost"){
         return io(BASE_URL)
     } else{
-        return io("/", {path:"/api/coket.io"})
+        return io("/", {path:"/api/socket.io"})
 
     }
 }
